a47408cff3f24ed6ca7f340fcb67e67c36c04f92
Colo <emir12345masi@gmail.com>
2023-05-02 11:18:38 -0300

    Realize la funcion add de PurchaseRepository.kt

--- src/main/kotlin/repositories/PurchaseRepository.kt
+++ src/main/kotlin/repositories/PurchaseRepository.kt
@@ -21,6 +21,7 @@ object PurchaseRepository {
 
     fun add(purchase: Purchase) {
         //TODO Implementar solucion para agregar una nueva compra
+        purchases.add(purchase)
     }
 
     fun get() : List<Purchase> {
